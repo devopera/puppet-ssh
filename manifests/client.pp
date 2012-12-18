5be2fbc978550937127add9ab9997081728d2d13
cleverlight <github.account@cleveraspaint.com>
2012-12-18 17:27:31 +0000

    added servers to client and factored package name out to params

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,5 +1,11 @@
 class ssh::client inherits ssh::params {
   package { 'openssh-client':
+    name => $client_package_name,
     ensure => present,
+  }
+  
+  file { '/etc/ssh/ssh_config':
+    content => template('ssh/ssh_config.erb'),
+    require => Package['openssh-client'],
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,9 +2,11 @@ class ssh::params {
   case $::osfamily {
     'Debian': {
       $service_name = 'ssh'
+      $client_package_name = 'openssh-client'
     }
     'RedHat': {
       $service_name = 'sshd'
+      $client_package_name = 'openssh'
     }
     default: {
       fail("Unsupported osfamily $::osfamily, currently only supports Debian and RedHat")
